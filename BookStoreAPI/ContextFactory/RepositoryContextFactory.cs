e40e0c94f0e6b0346b67db02f95a4f4ed4ab43dd
mustafa yucel <mustafaycl37@gmail.com>
2025-11-30 16:29:13 +0300

    RepositoryContextFactory added for migrations

---
+++ BookStoreAPI/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repositories.EFCore;
+
+namespace BookStoreAPI.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var optionsBuilder = new DbContextOptionsBuilder<RepositoryContext>()
+                .UseSqlServer(configuration.GetConnectionString("DefaultConnection"), prj => prj.MigrationsAssembly("BookStoreAPI"));
+            return new RepositoryContext(optionsBuilder.Options);
+        }
+    }
+}
